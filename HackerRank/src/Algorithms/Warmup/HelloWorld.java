d6ac142db377546cbd39f94413c7fe548f1ee3b7
Sayantan Guha <sayantanguha@hotmail.com>
2017-04-11 22:25:06 -0700

    Test

---
+++ HackerRank/src/Algorithms/Warmup/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Algorithms.Warmup;
+
+/**
+ * @author sayan
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
